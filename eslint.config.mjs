9db530339466233c3468a83c596e8d9572e0d615
Bobruiko Oleksii <bobruiko.oleksii@lll.kpi.ua>
2025-11-19 15:31:43 +0200

    chore: configure ESLint linter with flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+const compat = new FlatCompat();
+
+export default [
+  js.configs.recommended,
+  ...fixupConfigRules(compat.extends('airbnb-base')),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: fixupPluginRules(prettierPlugin),
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['**/node_modules/', '**/dist/'],
+  },
+];
